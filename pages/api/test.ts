206a17436f3917701010493b63e41c7a2612b8b9
Yi Zhao <genius.zhaoyi@gmail.com>
2021-03-19 04:35:51 -0400

    clean up

--- pages/api/test.ts
+++
@@ -1,8 +0,0 @@
-import { NextApiHandler } from 'next'
-
-const handler: NextApiHandler = async (req, res) => {
-    res.setHeader("ETag", "5cja7f7wbfb1c657e82dd4ssfj8sf0c6");
-    return res.send("Some data here");
-}
-
-export default handler;
